e63af4140b4e83391eb04b0cfd226ff26c35e85b
GoreToska <gorevoi_ivan@mail.ru>
2026-08-26 21:57:45 +0300

    Game module loading for windows

--- Engine/Source/Platform/Windows/WindowsDynamicLibrary.cpp
+++ Engine/Source/Platform/Windows/WindowsDynamicLibrary.cpp
@@ -6,7 +6,48 @@
 #include "Platform/Windows/WindowsDynamicLibrary.h"
 #include "Tools/Logger.h"
 
-namespace RTGDEngine {
+namespace RTGDEngine
+{
+    WindowsDynamicLibrary::~WindowsDynamicLibrary()
+    {
+        WindowsDynamicLibrary::Unload();
+    }
+
+    bool WindowsDynamicLibrary::Load(const std::string& fullPath)
+    {
+        Unload();
+
+        m_path = fullPath;
+
+        m_handle = LoadLibrary(fullPath.c_str());
+
+        if (!m_handle)
+        {
+            LogError("Failed to load dynamic library {}.", fullPath);
+            return false;
+        }
+
+        return true;
+    }
+
+    void WindowsDynamicLibrary::Unload()
+    {
+        if (m_handle)
+        {
+            FreeLibrary(m_handle);
+            m_handle = nullptr;
+        }
+    }
+
+    void* WindowsDynamicLibrary::GetSymbol(const std::string& name) const
+    {
+        return m_handle ? GetProcAddress(m_handle, name.c_str()) : nullptr;
+    }
+
+    bool WindowsDynamicLibrary::IsLoaded() const
+    {
+        return m_handle != nullptr;
+    }
 } // RTGDEditor
 
 #endif
